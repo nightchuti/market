06b70e4e62e6eed55d560ebea4ac5964042722d7
Chutimon <chutimonoumdesud@gmail.com>
2026-02-14 03:54:20 +0700

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,11 @@ import AllProducts from "./pages/AllProducts";
 import ProductDetail from "./pages/ProductDetail";
 import AddProduct from "./pages/AddProduct";
 import ChatPage from "./pages/ChatPage";
-<<<<<<< HEAD
-import ChatFloating from "./pages/ChatFloating";
-=======
-import MyShop from "./pages/MyShop/MyShop";
->>>>>>> 8432e73e143bb2f8927126ebc0d768bd99948ad8
+          import MyShop from "./pages/MyShop";
 
 function App() {
   return (
     <BrowserRouter>
-    <ChatFloating /> {/* แชทลอยตัวที่ทุกหน้าจะเห็น */}
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
@@ -22,11 +17,7 @@ function App() {
           <Route path="/products/:id" element={<ProductDetail />} />
           <Route path="/add-product" element={<AddProduct />} />
           <Route path="/chat/:sellerId" element={<ChatPage />} />
-<<<<<<< HEAD
-          
-=======
           <Route path="/my-shop" element={<MyShop />} />
->>>>>>> 8432e73e143bb2f8927126ebc0d768bd99948ad8
         </Route>
       </Routes>
     </BrowserRouter>
