ff73187bdf3d067b91acb2b34842b0b396d11f15
taparis <sirapat.w@ku.th>
2026-02-14 03:46:14 +0700

    feat(ChatPage): increase chat wrapper height for improved layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,12 @@ import AllProducts from "./pages/AllProducts";
 import ProductDetail from "./pages/ProductDetail";
 import AddProduct from "./pages/AddProduct";
 import ChatPage from "./pages/ChatPage";
+import ChatFloating from "./pages/ChatFloating";
 
 function App() {
   return (
     <BrowserRouter>
+    <ChatFloating /> {/* แชทลอยตัวที่ทุกหน้าจะเห็น */}
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
@@ -16,6 +18,7 @@ function App() {
           <Route path="/products/:id" element={<ProductDetail />} />
           <Route path="/add-product" element={<AddProduct />} />
           <Route path="/chat/:sellerId" element={<ChatPage />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
