6b51ec3570a7508504e728abbaaac27683ca6fbb
taparis <sirapat.w@ku.th>
2026-02-16 10:21:47 +0700

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-  headers: {
-    "Content-Type": "application/json"
-  }
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// แนบ token ทุก request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
