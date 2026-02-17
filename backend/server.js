72689821c6de028c9470687742d8558927ce873b
Chutimon <chutimonoumdesud@gmail.com>
2026-02-18 04:30:56 +0700

    server

--- backend/server.js
+++ backend/server.js
@@ -65,6 +65,7 @@ app.use("/api/messages", messageRoutes);
 
 // ===== START SERVER =====
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
