b875d8a0e9805bb933d79b29bc17ed56d8e44d5e
taparis <sirapat.w@ku.th>
2026-02-19 03:15:47 +0700

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://testt-zu9t.onrender.com",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
