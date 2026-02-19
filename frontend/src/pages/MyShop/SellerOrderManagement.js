5e6beb098afcd08b4f4ec4a4a20da17068bd0850
Chutimon <chutimonoumdesud@gmail.com>
2026-02-20 02:36:05 +0700

    css

--- frontend/src/pages/MyShop/SellerOrderManagement.js
+++ frontend/src/pages/MyShop/SellerOrderManagement.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import "./SellerOrders.css";
+import "./SellerOrderManagement.css";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
