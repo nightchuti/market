8fc3630767705cee09ba362522833a1d39e229b3
Chutimon <chutimonoumdesud@gmail.com>
2026-02-21 01:32:26 +0700

    backrnd

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -75,7 +75,7 @@ router.get("/seller/all", protect, async (req, res) => {
       .populate({
         path: "items.product",
         model: "Product",
-        select: "title price user",  // ดึงข้อมูล seller มาด้วยเพื่อกรอง
+        select: "title price user images",  // ดึงข้อมูล seller มาด้วยเพื่อกรอง
       })
       .sort({ createdAt: -1 });
 
